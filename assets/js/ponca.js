30f7d6d4b0df4aa64af66202e08ddd76eb772b56
Carson Smith <90855545+casmith18@users.noreply.github.com>
2024-04-16 19:24:06 +0000

    worked on chartjs

--- assets/js/ponca.js
+++ assets/js/ponca.js
@@ -16,49 +16,48 @@ PID's can be found at https://maps.waterdata.usgs.gov/mapper/
 	00065 - gauge height
 */
 async function GetPonca() {
-    var myURL =
-      "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055660&period=P7D&parameterCd=00065&siteStatus=active";
-    var msgObject = await fetch(myURL);
-    var msgJSONText = await msgObject.text();
-    var msg = JSON.parse(msgJSONText);
-    document.getElementById("msg").innerHTML = msgJSONText;
-  
-    /* Site 1 */
-    var dates = [];
-    var values = [];
-    /* fLen contains the length of the array (number of values) */
-    fLen = msg.value.timeSeries[0].values[0].value.length;
-    for (i = 0; i < fLen; i++) {
-      values[i] = msg.value.timeSeries[0].values[0].value[i].value;
-      dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime;
-    }
-    var sitename = msg.value.timeSeries[0].sourceInfo.siteName;
-    var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
-    var siteDescription = msg.value.timeSeries[0].variable.variableDescription;
-  
-    /* Put your code here to display a graph of values and dates for Site 1*/
-  
-    var ctx = document.getElementById("chartjs-0");
-  
-    var myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: dates,
-        datasets: [
-          {
-            label:
-              siteDescription + " at " + sitename,
-            data: values,
-            fill: false,
-            borderColor: "blue",
-            lineTension: 0.1,
-          },
-        ],
-      },
-      options: {
-        responsive: false,
-        maintainAspectRatio: true,
-      },
-    });    
-  
+  var myURL =
+    "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055660&period=P7D&parameterCd=00065&siteStatus=active";
+  var msgObject = await fetch(myURL);
+  var msgJSONText = await msgObject.text();
+  var msg = JSON.parse(msgJSONText);
+  document.getElementById("msg").innerHTML = msgJSONText;
+
+  /* Site 1 */
+  var dates = [];
+  var values = [];
+  /* fLen contains the length of the array (number of values) */
+  fLen = msg.value.timeSeries[0].values[0].value.length;
+  for (i = 0; i < fLen; i++) {
+    values[i] = msg.value.timeSeries[0].values[0].value[i].value;
+    dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime;
   }
+  var sitename = msg.value.timeSeries[0].sourceInfo.siteName;
+  var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
+  var siteDescription = msg.value.timeSeries[0].variable.variableDescription;
+
+  /* Put your code here to display a graph of values and dates for Site 1*/
+
+  var ctx = document.getElementById("chartjs-0");
+
+  var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: siteDescription + " at " + sitename,
+          data: values,
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+          
+        },
+      ],
+    },
+    options: {
+      responsive: false,
+      maintainAspectRatio: true,
+    },
+  });
+}
